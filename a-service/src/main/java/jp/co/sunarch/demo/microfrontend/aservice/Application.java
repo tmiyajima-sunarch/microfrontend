47855212688459a989c28dd19c036b6859291867
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2019-07-26 03:40:42 +0900

    a-serviceのコンポーネントでAPIを呼び出すように修正

--- a-service/src/main/java/jp/co/sunarch/demo/microfrontend/aservice/Application.java
+++ a-service/src/main/java/jp/co/sunarch/demo/microfrontend/aservice/Application.java
@@ -2,12 +2,23 @@ package jp.co.sunarch.demo.microfrontend.aservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/api")
+	public String api() throws InterruptedException {
+		Thread.sleep(3000);
+		return "OK";
 	}
 
 }
--- a-service/src/main/resources/static/js/a-component.js
+++ a-service/src/main/resources/static/js/a-component.js
@@ -1,22 +1,69 @@
-const AComponent = {
-  props: {
-    message: {
-      type: String,
-      default: '(No message)'
+(function() {
+  const AComponent = {
+    props: {
+      message: {
+        type: String,
+        default: '(No message)'
+      },
+      baseUrl: {
+        type: String,
+        required: true
+      }
+    },
+    template: `
+      <div>
+        <p>ここは、a-serviceから配信されているJavaScriptで描画されています。</p>
+        <p>以下のパラメータがコンテナ画面より渡されています。</p>
+        <pre class="bg-dark text-white p-3">{{ JSON.stringify($props, null, 2) }}</pre>
+        <button class="btn btn-primary" type="button" :disabled="loading" @click="callApi()">
+          <template v-if="loading">
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            <span class="sr-only">Loading...</span>
+          </template>
+          APIを呼び出す
+        </button>
+        <pre class="bg-dark text-white p-3 mt-3 mb-0" v-if="result">{{ JSON.stringify(result, null, 2) }}</pre>
+      </div>
+    `,
+    data() {
+      return {
+        loading: false,
+        result: null
+      }
+    },
+    methods: {
+      async callApi() {
+        this.loading = true
+        this.result = null
+        try {
+          const res = await fetch(`${this.baseUrl}/api`, {
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8'
+            }
+          })
+          this.result = {
+            response: await res.text()
+          }
+        } catch (e) {
+          this.result = {
+            error: true,
+            message: e.message
+          }
+        } finally {
+          this.loading = false
+        }
+      }
     }
-  },
-  template: `
-    <h1>This is A-Component: Hello {{ message }}</h1>
-  `
-}
+  }
 
-window.renderAComponent = (el, options) => {
-  new Vue({
-    el,
-    render(h) {
-      return h(AComponent, {
-        props: options
-      })
-    }
-  })
-}
+  window.renderAComponent = (el, options) => {
+    new Vue({
+      el,
+      render(h) {
+        return h(AComponent, {
+          props: options
+        })
+      }
+    })
+  }
+})()
