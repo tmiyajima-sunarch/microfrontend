02ce7cff79ab938139cea453ad4a58b8594c0090
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2019-07-27 09:48:37 +0900

    コールバックの例を追加

--- b-service/src/main/resources/static/js/b-component.js
+++ b-service/src/main/resources/static/js/b-component.js
@@ -1,10 +1,18 @@
 window.renderBComponent = (el, options) => {
-  $(el).append($(`
+  const div = $(`
     <div>
-      <p>ここは、b-serviceにより描画されています。</p>
-      <p>jQueryにより、DOMを生成して描画しています。</p>
+      <p>ここは、b-serviceにより、jQueryで描画されています。</p>
       <p>以下のパラメータがコンテナ画面より渡されています。</p>
       <pre class="p-3 bg-dark text-white">${JSON.stringify(options, null, 2)}</pre>
+      <button class="btn btn-primary" type="button">コンテナ画面のコールバックを呼び出す</button>
     </div>
-  `))
+  `)
+
+  div.find('button').click(() => {
+    if (options.onClick) {
+      options.onClick();
+    }
+  })
+
+  div.appendTo(el)
 }
