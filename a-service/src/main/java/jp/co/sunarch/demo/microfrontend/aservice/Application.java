85fc9b0d37b26db222d65bccb6ad6adbdb41da73
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2019-07-28 02:54:19 +0900

    描画方法を変更
    参考：https://github.com/micro-frontends-demo/container/blob/master/src/MicroFrontend.js

--- a-service/src/main/java/jp/co/sunarch/demo/microfrontend/aservice/Application.java
+++ a-service/src/main/java/jp/co/sunarch/demo/microfrontend/aservice/Application.java
@@ -2,17 +2,28 @@ package jp.co.sunarch.demo.microfrontend.aservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
-@CrossOrigin
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 	@GetMapping("/api")
--- container-app/src/main/java/jp/co/sunarch/demo/microfrontend/containerapp/Application.java
+++ container-app/src/main/java/jp/co/sunarch/demo/microfrontend/containerapp/Application.java
@@ -2,6 +2,7 @@ package jp.co.sunarch.demo.microfrontend.containerapp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,9 +19,12 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("themes", Arrays.asList(
+	private static final List<MicroFrontend> MICRO_FRONTENDS = Arrays.asList(
+		new MicroFrontend("a-component", "AComponent", "http://localhost:8081"),
+		new MicroFrontend("b-component", "BComponent", "http://localhost:8082")
+	);
+
+	private static final List<String> THEMES = Arrays.asList(
 			"default",
 			"cerulean",
 			"cosmo",
@@ -43,8 +47,12 @@ public class Application {
 			"superhero",
 			"united",
 			"yeti"
-		));
-		model.addAttribute("components", Arrays.asList("a-component", "b-component"));
+		);
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("themes", THEMES);
+		model.addAttribute("components", microFrontendIds());
 		return "index";
 	}
 
@@ -54,8 +62,46 @@ public class Application {
 		@RequestParam(name = "theme", defaultValue = "default") String theme,
 		@RequestParam(name = "components", defaultValue = "a-component,b-component") List<String> components) {
 		model.addAttribute("theme", theme);
-		model.addAttribute("components", components);
+		model.addAttribute("microFrontends",
+				components.stream().map(this::resolve).collect(Collectors.toList()));
 		return "main";
+	}
+
+	private List<String> microFrontendIds() {
+		return MICRO_FRONTENDS.stream()
+				.map(MicroFrontend::getId)
+				.collect(Collectors.toList());
+	}
+
+	private MicroFrontend resolve(String id) {
+		return MICRO_FRONTENDS.stream()
+				.filter(m -> m.getId().equals(id))
+				.findFirst()
+				.orElse(null);
+	}
+
+	public static class MicroFrontend {
+		private final String id;
+		private final String name;
+		private final String url;
+		
+		public MicroFrontend(String id, String name, String url) {
+			this.id = id;
+			this.name = name;
+			this.url = url;
+		}
+
+		public String getId() {
+			return this.id;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public String getUrl() {
+			return this.url;
+		}
 	}
 
 }
